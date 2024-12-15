21f24ab93c7c603f6cde9f8fe81fe35c28bbb0b6
Brett <bretttucker@gmail.com>
2024-12-14 17:08:36 -0700

    Implement the splash screen

--- app/src/main/java/dev/bltucker/mastermeme/MainActivity.kt
+++ app/src/main/java/dev/bltucker/mastermeme/MainActivity.kt
@@ -11,37 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.bltucker.mastermeme.ui.theme.MasterMemeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MasterMemeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Text("Todo: App Goes here")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MasterMemeTheme {
-        Greeting("Android")
-    }
-}
